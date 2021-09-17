37c34b3bd6f2e6952d89136a55db82cfcac90ff3
astratow <davno_pora@mail.com>
2021-09-17 04:53:08 +0100

    index.php updated

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) )
   <h1 class="page-title"><?php single_post_title(); ?> </h1>
 </header>
 function() {
-  echo '<h1>This is myNewThem.</h1>';
+  echo '<h1>This is myNewTheme.</h1>';
 }
 
 ?>
