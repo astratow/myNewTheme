66d2e0638ac393afbc310abefd5e705c5b99a666
astratow <davno_pora@mail.com>
2021-09-22 05:28:25 +0100

    started to implemate bootstrap nav walker

--- functions.php
+++ functions.php
@@ -1,3 +1,18 @@
 /*
 *This is functions file for myNewTheme WordPress theme
 */
+<?php
+	/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+	// Theme Support
+	function wpb_theme_setup(){
+		// Nav Menus
+		register_nav_menus( array(
+    	'primary' => __( 'Primary Menu', 'THEMENAME' ),
+	));
+	}
