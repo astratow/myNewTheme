afa9cb07157cceafa43e2abf899f8670b8706538
astratow <davno_pora@mail.com>
2021-10-18 05:01:08 +0100

    bootstrap-5.1.3 moved into css and js folders, bootstrap folder removed

--- functions.php
+++ functions.php
@@ -17,5 +17,21 @@ add_action( 'after_setup_theme', 'register_navwalker' );
 	));
   }
 function load_css(){
-  wp_register_style('')
+  wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false /*version*/, 'all'); 
+    /*get_template_directory() 
+     *wp_register_style()
+     *array()
+     * */
+    wp_enqueue_style('bootstrap');
+}
+add_action('wp_enqueue_scripts', 'load_css');
+
+
+function load_js(){
+  wp_enqueue_script('jquery');
+  wp_register_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true);
+  wp_enqueue_script('bootstrapjs');
+}
+add_action('wp_enqueue_scripts', 'load_js');
+    
 ?>
